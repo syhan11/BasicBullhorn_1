d11403b8a7249f5fadc22054b260353dad269a99
Sue <sueyoung.6311@gmail.com>
2019-04-17 19:53:10 -0400

    first working version

--- src/main/java/com/byhorm/basicbullhorn_1/HomeController.java
+++ src/main/java/com/byhorm/basicbullhorn_1/HomeController.java
@@ -3,8 +3,13 @@ package com.byhorm.basicbullhorn_1;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class HomeController {
@@ -15,5 +20,27 @@ public class HomeController {
     public String listMsg (Model model) {
         model.addAttribute("messages", msgRepository.findAll());
         return "list";
+    }
+
+    @GetMapping("/add")
+    public String msgForm(Model model){
+        model.addAttribute("message", new Message());
+        return "msgform";
+    }
+
+    @PostMapping("/process")
+    public String processMsg(@Valid Message message,
+                             BindingResult result) {
+        if (result.hasErrors()) {
+            return "msgform";
+        }
+        msgRepository.save(message);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showMsg(@PathVariable("id") long id, Model model) {
+        model.addAttribute("message", msgRepository.findById(id).get());
+        return "show";
     }
 }
